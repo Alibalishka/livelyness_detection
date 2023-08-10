ac2552d5bda6eb4ef396dc17d7308c3a9b46f8fd
Alibalishka <groznyi.alik@gmail.com>
2023-08-10 14:23:40 +0600

    update ml face version

--- lib/src/core/extensions/ml_kit_extesion.dart
+++ lib/src/core/extensions/ml_kit_extesion.dart
@@ -3,38 +3,29 @@ import 'package:m7_livelyness_detection/index.dart';
 extension MLKitUtils on AnalysisImage {
   InputImage toInputImage() {
     final planeData =
-        when(nv21: (img) => img.planes, bgra8888: (img) => img.planes)?.map(
-      (plane) {
-        return InputImagePlaneMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          height: height,
-          width: width,
-        );
-      },
-    ).toList();
+        when(nv21: (img) => img.planes, bgra8888: (img) => img.planes);
 
     return when(nv21: (image) {
       return InputImage.fromBytes(
         bytes: image.bytes,
-        inputImageData: InputImageData(
-          imageRotation: inputImageRotation,
-          inputImageFormat: InputImageFormat.nv21,
-          planeData: planeData,
-          size: image.size,
-        ),
+        metadata: InputImageMetadata(
+            rotation: inputImageRotation,
+            format: InputImageFormat.nv21,
+            // planeData: planeData,
+            size: image.size,
+            bytesPerRow: planeData!.first.bytesPerRow),
       );
     }, bgra8888: (image) {
-      final inputImageData = InputImageData(
-        size: size,
-        // FIXME: seems to be ignored on iOS...
-        imageRotation: inputImageRotation,
-        inputImageFormat: inputImageFormat,
-        planeData: planeData,
-      );
+      final inputImageData = InputImageMetadata(
+          size: size,
+          // FIXME: seems to be ignored on iOS...
+          rotation: inputImageRotation,
+          format: inputImageFormat,
+          bytesPerRow: planeData!.first.bytesPerRow);
 
       return InputImage.fromBytes(
         bytes: image.bytes,
-        inputImageData: inputImageData,
+        metadata: inputImageData,
       );
     })!;
   }
