be6d8e25988957f5e7cbfedf6515c673c70e3f74
Alibalishka <groznyi.alik@gmail.com>
2024-01-21 15:21:24 +0600

    discard changes

--- example/lib/screens/example_face_detaticon.dart
+++ example/lib/screens/example_face_detaticon.dart
@@ -9,73 +9,115 @@ class FaceDetectionPage extends StatefulWidget {
 }
 
 class _FaceDetectionPageState extends State<FaceDetectionPage> {
-  final List<M7LivelynessStepItem> veificationSteps = [
-    M7LivelynessStepItem(
-      step: M7LivelynessStep.blink,
-      title: "Blink",
-      isCompleted: false,
-      detectionColor: Colors.amber,
-    ),
-    M7LivelynessStepItem(
-      step: M7LivelynessStep.smile,
-      title: "Smile",
-      isCompleted: false,
-      detectionColor: Colors.green.shade800,
-    ),
-  ];
+  final List<M7LivelynessStepItem> _veificationSteps = [];
 
-  void initilizeDetection() => M7LivelynessDetection.instance.configure(
-        lineColor: Colors.white,
-        dotColor: Colors.purple.shade800,
-        dotSize: 2.0,
-        lineWidth: 2,
-        dashValues: [2.0, 5.0],
-        displayDots: false,
-        displayLines: false,
-        thresholds: [
-          M7SmileDetectionThreshold(probability: 0.8),
-          M7BlinkDetectionThreshold(
-            leftEyeProbability: 0.25,
-            rightEyeProbability: 0.25,
-          ),
-        ],
-      );
+  void _initValues() {
+    _veificationSteps.addAll(
+      [
+        M7LivelynessStepItem(
+          step: M7LivelynessStep.smile,
+          title: "Smile",
+          isCompleted: false,
+        ),
+        M7LivelynessStepItem(
+          step: M7LivelynessStep.blink,
+          title: "Blink",
+          isCompleted: false,
+        ),
+      ],
+    );
+    M7LivelynessDetection.instance.configure(
+      lineColor: Colors.transparent,
+      dotColor: Colors.transparent,
+      thresholds: [
+        M7SmileDetectionThreshold(probability: 0.8),
+        M7BlinkDetectionThreshold(
+          leftEyeProbability: 0.85,
+          rightEyeProbability: 0.85,
+        ),
+      ],
+    );
+  }
 
   void _onStartLivelyness(context) async {
     final M7CapturedImage? response =
         await M7LivelynessDetection.instance.detectLivelyness(
       context,
-      appBar: AppBar(),
-      primaryColor: const Color.fromRGBO(229, 101, 83, 1),
       config: M7DetectionConfig(
-        steps: veificationSteps,
+        steps: _veificationSteps,
         startWithInfoScreen: false,
         maxSecToDetect: 2500,
         allowAfterMaxSec: false,
-        captureButtonColor: const Color.fromRGBO(229, 101, 83, 1),
+        captureButtonColor: Colors.blue,
       ),
-      text: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 22),
+      text: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22),
         child: Text(
           'Пожалуйста, расположите свое лицо в овал',
           textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+              ),
         ),
       ),
     );
     if (response == null) {
       return;
     }
-    print('ALI IMAGE:' + response.imgPath);
-    // model.uploadImageIdentificationSelfie(File(response.imgPath));
-    // Navigator.push(
-    //     context,
-    //     MaterialPageRoute(
-    //         builder: (context) => const DocumentPictureFrontWidget()));
   }
+  // void initilizeDetection() => M7LivelynessDetection.instance.configure(
+  //       lineColor: Colors.white,
+  //       dotColor: Colors.purple.shade800,
+  //       dotSize: 2.0,
+  //       lineWidth: 2,
+  //       dashValues: [2.0, 5.0],
+  //       displayDots: false,
+  //       displayLines: false,
+  //       thresholds: [
+  //         M7SmileDetectionThreshold(probability: 0.8),
+  //         M7BlinkDetectionThreshold(
+  //           leftEyeProbability: 0.25,
+  //           rightEyeProbability: 0.25,
+  //         ),
+  //       ],
+  //     );
+
+  // void _onStartLivelyness(context) async {
+  //   final M7CapturedImage? response =
+  //       await M7LivelynessDetection.instance.detectLivelyness(
+  //     context,
+  //     appBar: AppBar(),
+  //     primaryColor: const Color.fromRGBO(229, 101, 83, 1),
+  //     config: M7DetectionConfig(
+  //       steps: veificationSteps,
+  //       startWithInfoScreen: false,
+  //       maxSecToDetect: 2500,
+  //       allowAfterMaxSec: false,
+  //       captureButtonColor: const Color.fromRGBO(229, 101, 83, 1),
+  //     ),
+  //     text: const Padding(
+  //       padding: EdgeInsets.symmetric(horizontal: 22),
+  //       child: Text(
+  //         'Пожалуйста, расположите свое лицо в овал',
+  //         textAlign: TextAlign.center,
+  //       ),
+  //     ),
+  //   );
+  //   if (response == null) {
+  //     return;
+  //   }
+  //   print('ALI IMAGE:' + response.imgPath);
+  //   // model.uploadImageIdentificationSelfie(File(response.imgPath));
+  //   // Navigator.push(
+  //   //     context,
+  //   //     MaterialPageRoute(
+  //   //         builder: (context) => const DocumentPictureFrontWidget()));
+  // }
 
   @override
   void initState() {
-    initilizeDetection();
+    _initValues();
     super.initState();
   }
 
--- example/lib/screens/example_screen.dart
+++ example/lib/screens/example_screen.dart
@@ -77,10 +77,10 @@ class _M7ExpampleScreenState extends State<M7ExpampleScreen> {
     final M7CapturedImage? response =
         await M7LivelynessDetection.instance.detectLivelyness(
       context,
-      primaryColor: const Color.fromRGBO(229, 101, 83, 1),
-      styleTextHeader: TextStyle(),
+      // primaryColor: const Color.fromRGBO(229, 101, 83, 1),
+      // styleTextHeader: TextStyle(),
       text: Text('data'),
-      appBar: AppBar(),
+      // appBar: AppBar(),
       config: M7DetectionConfig(
         steps: _veificationSteps,
         startWithInfoScreen: false,
